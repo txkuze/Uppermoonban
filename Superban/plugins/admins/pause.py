ab0515f05306e45af82fe1105639ea4262949ac2
UCHIHABOTS <txkuze@gmail.com>
2026-02-04 17:22:26 +0530

    Update pause.py

--- Superban/plugins/admins/pause.py
+++ Superban/plugins/admins/pause.py
@@ -2,11 +2,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
-from Oneforall import app
-from Oneforall.core.call import Hotty
-from Oneforall.utils.database import is_music_playing, music_off
-from Oneforall.utils.decorators import AdminRightsCheck
-from Oneforall.utils.inline import close_markup
+from Superban import app
+from Superban.core.call import Superban
+from Superban.utils.database import is_music_playing, music_off
+from Superban.utils.decorators import AdminRightsCheck
+from Superban.utils.inline import close_markup
 
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Hotty.pause_stream(chat_id)
+    await Superban.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
